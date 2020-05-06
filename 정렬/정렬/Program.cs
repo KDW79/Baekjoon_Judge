79a1802568f8d1fb9ffe579c17b08bdbb7e27e1b
KDW79 <no1holycow@gmail.com>
2020-05-06 11:07:40 +0900

    test

---
+++ 정렬/정렬/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10989_수_정렬하기3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            List<int> myArray = new List<int>();
+
+            for (int i = 0; i < N; i++)
+            {
+                myArray.Add(int.Parse(Console.ReadLine()));
+            }
+            myArray.Sort();
+
+            foreach (int a in myArray)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
