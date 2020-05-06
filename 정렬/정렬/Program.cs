0ba2fe1d855f7a3a4fa404f0baa732dc50c90071
KDW79 <no1holycow@gmail.com>
2020-05-06 12:35:10 +0900

    메모리초과 하지만, 문제가 원하는대로 정렬은 됨. 배열의 초기 갯수 설정이 문제로 생각됨

--- 정렬/정렬/Program.cs
+++ 정렬/정렬/Program.cs
@@ -12,17 +12,53 @@ namespace _10989_수_정렬하기3
         {
             int N = int.Parse(Console.ReadLine());
 
-            List<int> myArray = new List<int>();
+            //List<int> myArray = new List<int>();
+
+            //for (int i = 0; i < N; i++)
+            //{
+            //    myArray.Add(int.Parse(Console.ReadLine()));
+            //}
+
+            //myArray.Sort(); // 메모리 초과 발생됨
+
+            //foreach (int a in myArray)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            int[] myArray = Enumerable.Repeat<int>(0,10000000).ToArray<int>();
+
+
+            int tmp = 0;
+
+            //Console.WriteLine("N : {0}",N);
 
             for (int i = 0; i < N; i++)
             {
-                myArray.Add(int.Parse(Console.ReadLine()));
+               myArray[i] = int.Parse(Console.ReadLine());
             }
-            myArray.Sort();
 
-            foreach (int a in myArray)
+            int cnt = 0;
+            while (cnt <= N)
             {
-                Console.WriteLine(a);
+                for (int i= N - 1; i > cnt; i--)
+                {
+                    //Console.WriteLine("tmp : , i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
+                    tmp = myArray[i];
+                    if (myArray[i] < myArray[i - 1]) // 이전 배열 수가 더 크면 둘이 바꿈
+                    {
+                        myArray[i] = myArray[i - 1];
+                        myArray[i - 1] = tmp;
+                    }
+                    //Console.WriteLine("tmp :{0}, i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
+                }
+                //Console.WriteLine("=========================================");
+                cnt++;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(myArray[i]);
             }
         }
     }
