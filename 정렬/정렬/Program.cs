2e53abbc178ea67b2b5f98cf8f89ff1f23eb68c7
KDW79 <no1holycow@gmail.com>
2020-05-07 11:43:31 +0900

    메모리 초과는 통과 했으나 시간 초과 발생

--- 정렬/정렬/Program.cs
+++ 정렬/정렬/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _10989_수_정렬하기3
 {
@@ -10,56 +11,78 @@ namespace _10989_수_정렬하기3
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
+            //int N = int.Parse(Console.ReadLine());
 
-            //List<int> myArray = new List<int>();
+            ////List<int> myArray = new List<int>();
+
+            ////for (int i = 0; i < N; i++)
+            ////{
+            ////    myArray.Add(int.Parse(Console.ReadLine()));
+            ////}
+
+            ////myArray.Sort(); // 메모리 초과 발생됨
+
+            ////foreach (int a in myArray)
+            ////{
+            ////    Console.WriteLine(a);
+            ////}
+
+            //int[] myArray = Enumerable.Repeat<int>(0,10000).ToArray<int>();
+
+
+            //int tmp = 0;
+
+            ////Console.WriteLine("N : {0}",N);
 
             //for (int i = 0; i < N; i++)
             //{
-            //    myArray.Add(int.Parse(Console.ReadLine()));
+            //   myArray[i] = int.Parse(Console.ReadLine());
             //}
 
-            //myArray.Sort(); // 메모리 초과 발생됨
-
-            //foreach (int a in myArray)
+            //int cnt = 0;
+            //while (cnt <= N)
             //{
-            //    Console.WriteLine(a);
+            //    for (int i= N - 1; i > cnt; i--)
+            //    {
+            //        //Console.WriteLine("tmp : , i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
+            //        tmp = myArray[i];
+            //        if (myArray[i] < myArray[i - 1]) // 이전 배열 수가 더 크면 둘이 바꿈
+            //        {
+            //            myArray[i] = myArray[i - 1];
+            //            myArray[i - 1] = tmp;
+            //        }
+            //        //Console.WriteLine("tmp :{0}, i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
+            //    }
+            //    //Console.WriteLine("=========================================");
+            //    cnt++;
             //}
 
-            int[] myArray = Enumerable.Repeat<int>(0,10000000).ToArray<int>();
+            //for (int i = 0; i < N; i++)
+            //{
+            //    Console.WriteLine(myArray[i]);
+            //}
 
+            //StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            //StringBuilder sb = new StringBuilder();
 
-            int tmp = 0;
+            int N = int.Parse(Console.ReadLine());
+            int[] b = new int[10001];
 
-            //Console.WriteLine("N : {0}",N);
+            for (int i = 0; i < N; i++) ++b[int.Parse(Console.ReadLine())];
 
-            for (int i = 0; i < N; i++)
+            for (int i = 1; i < 10001; i++)
             {
-               myArray[i] = int.Parse(Console.ReadLine());
-            }
-
-            int cnt = 0;
-            while (cnt <= N)
-            {
-                for (int i= N - 1; i > cnt; i--)
+                int j;
+                //for (; b[i]>0; b[i]--)
+                //{
+                //    sb.AppendFormat("{0}\n", i);
+                //}
+                if (b[i] > 0)
                 {
-                    //Console.WriteLine("tmp : , i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
-                    tmp = myArray[i];
-                    if (myArray[i] < myArray[i - 1]) // 이전 배열 수가 더 크면 둘이 바꿈
-                    {
-                        myArray[i] = myArray[i - 1];
-                        myArray[i - 1] = tmp;
-                    }
-                    //Console.WriteLine("tmp :{0}, i-1 :{1}, i :{2}", tmp, myArray[i - 1], myArray[i]);
+                    for (j = 0; j < b[i]; j++) Console.WriteLine(i);
                 }
-                //Console.WriteLine("=========================================");
-                cnt++;
             }
-
-            for (int i = 0; i < N; i++)
-            {
-                Console.WriteLine(myArray[i]);
-            }
+            //Console.WriteLine(sb.ToString());
         }
     }
 }
